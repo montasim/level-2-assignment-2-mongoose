c6a029c6a602d9059eac191a104dfcad9d246827
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-05-23 23:26:02 +0600

    task 4

---
+++ Task-4/src/app/modules/user/book.model.ts
@@ -0,0 +1,48 @@
+import {Schema, model, Model, Query} from 'mongoose';
+import { IBook} from "./book.interface";
+
+const BookSchema = new Schema<IBook>({
+    title: { type: String, required: true },
+    author: { type: [String], required: true },
+    genre: { type: String, required: true },
+    publicationYear: { type: Number, required: true },
+    publisher: {
+        name: { type: String, required: true },
+        location: { type: String, required: true },
+    },
+    reviews: [
+        {
+            user: { type: String, required: true },
+            comment: { type: String, required: true },
+        },
+    ],
+    rating: { type: Number, required: true },
+    price: { type: String, required: true },
+});
+
+interface IBookModel extends Model<IBook> {
+    findFeaturedBooks(): Query<IBook[], IBook>;
+}
+
+BookSchema.statics.findFeaturedBooks = async function (): Promise<IBook[]> {
+    try {
+        const featuredBooks = await this.find({ rating: { $gte: 4 } });
+
+        featuredBooks.forEach((book) => {
+            if (book.rating >= 4.5) {
+                book.featured = 'BestSeller';
+            } else {
+                book.featured = 'Popular';
+            }
+        });
+
+        return featuredBooks;
+    } catch (error) {
+        // Handle error
+        console.error('Error finding featured books:', error);
+        throw error;
+    }
+};
+const Book = model<IBook, IBookModel>('Book', BookSchema);
+
+export { Book };
---
+++ Task-4/src/app/modules/user/book.route.ts
@@ -0,0 +1,9 @@
+let express = require('express');
+import {findBooksByGenreAndPublisher, findBooksUsingGenre, findFeaturedBooks} from './book.controller';
+const router = express.Router();
+
+router.get('/genre/:genre', findBooksUsingGenre);
+router.get('/genre/:genre/publisher/:publisher', findBooksByGenreAndPublisher);
+router.get('getFeaturedBooks', findFeaturedBooks)
+
+export default router;
