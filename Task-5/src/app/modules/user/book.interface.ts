788c15a331c3f95c31e406467f41cab4390bb359
Ｍ♢ＮＴΛＳＩＭ <montasimmamun@gmail.com>
2023-05-23 23:26:26 +0600

    task 5

---
+++ Task-5/src/app/modules/user/book.interface.ts
@@ -0,0 +1,19 @@
+export interface IBook {
+  title: string;
+  author: string[];
+  genre: string;
+  publicationYear: number;
+  publisher: {
+    name: string;
+    location: string;
+  };
+  reviews: [
+    {
+      user: string;
+      comment: string;
+    }
+  ];
+  rating: number;
+  price: string;
+  featured?: string;
+}
